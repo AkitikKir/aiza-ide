660afb7bff5a1cb3086f391d82ee9515b0b4cf8e
AkitikKir <akitikkir@gmail.com>
2026-01-08 09:17:09 +0000

    d0.0.0.1 build fix

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization")
 }
 
 kotlin {
@@ -9,6 +10,7 @@ kotlin {
             dependencies {
                 implementation(project(":core"))
                 implementation(project(":config"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
             }
         }
